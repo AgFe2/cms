f1bb281a837bd11a03202a63dfa119c9e5edbc7e
You <duug@naver.com>
2022-10-11 15:44:32 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요.

--- user-api/src/test/java/com/AgFe2/cms/user/client/service/EmailSendServiceTest.java
+++ user-api/src/test/java/com/AgFe2/cms/user/client/service/EmailSendServiceTest.java
@@ -1,7 +1,7 @@
 package com.AgFe2.cms.user.client.service;
 
-import com.AgFe2.cms.user.client.service.test.EmailSendService;
-import com.AgFe2.cms.user.config.FeignConfig;
+import com.AgFe2.cms.user.client.MailgunClient;
+import com.AgFe2.cms.user.client.mailgun.SendMailForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,12 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EmailSendServiceTest {
 
     @Autowired
-    private EmailSendService emailSendService;
+    private MailgunClient mailgunClinet;
 
     @Test
     public void EmailTest () {
-        String response = emailSendService.sendEmail();
-        System.out.println(response);
-    }
+        // need test code
+        SendMailForm form = SendMailForm.builder()
+                .from("frog5667@gmail.com")
+                .to("duug@naver.com")
+                .subject("Test email from AgFe2")
+                .text("my text")
+                .build();
 
+        mailgunClinet.sendEmail(form);
+//        String response = mailgunClinet.sendEmail();
+//        System.out.println(response);
+
+    }
 }
